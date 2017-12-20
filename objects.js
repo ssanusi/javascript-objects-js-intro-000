10c4ae3030f79646173ee291537d6ce8755ad27a
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-20 20:46:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {'2face':'African Queen'};
 function updatePlaylist(playlist, artistName, songTitle){
   Object.assign(playlist, { artistName : songTitle});
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  
+}
